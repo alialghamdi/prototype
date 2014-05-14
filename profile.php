e338c422aadb12267c0f46c79808724028709b29
Ali Alghamdi <ali@tkmeel.com>
2014-05-14 23:11:17 +0300

    Added a page.
    viewPlan => seeing a student’s plan.

--- profile.php
+++ profile.php
@@ -14,7 +14,6 @@
     <div class="container">
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
-        
         <div class="row">
             <div class="col-md-6">
                 <h3>Ali Alghamdi</h3>
---
+++ viewPlan.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>KFUPM Academic Planner - Profile</title>
+     <?php
+        include("head.php");
+      ?>
+  </head>
+
+  <body>
+      <?php
+        include("studentNav.php");
+      ?>
+    <div class="container">
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <div class="row">
+            <div class="col-md-8">
+                <h3>Plan:</h3>
+                <br>
+                <table class="table table-striped">
+                    <tr>
+                        <td>Semster</td>
+                        <td>Courses</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>MATH001</td>
+                        <td>ENGL001</td>
+                        <td>PYP003</td>
+                        <td>PE001</td>
+                        <td>PYP001</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>MATH002</td>
+                        <td>ENGL002</td>
+                        <td>PYP003</td>
+                        <td>PE001</td>
+                        <td>PYP001</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>MATH002</td>
+                        <td>ENGL002</td>
+                        <td>PYP003</td>
+                        <td>PE001</td>
+                        <td>PYP001</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>MATH002</td>
+                        <td>ENGL002</td>
+                        <td>PYP003</td>
+                        <td>PE001</td>
+                        <td>PYP001</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>MATH002</td>
+                        <td>ENGL002</td>
+                        <td>PYP003</td>
+                        <td>PE001</td>
+                        <td>PYP001</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>MATH002</td>
+                        <td>ENGL002</td>
+                        <td>PYP003</td>
+                        <td>PE001</td>
+                        <td>PYP001</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>MATH002</td>
+                        <td>ENGL002</td>
+                        <td>PYP003</td>
+                        <td>PE001</td>
+                        <td>PYP001</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+                <button type="editPlan.php" class="btn btn-primary btn-lg btn-block">Edit Plan</button>
+            </div>
+            <div class="col-md-4">
+                
+            </div>
+            <div class="col-md-8"></div>
+            <div class="col-md-4">
+                <h3>Comments</h3>
+            </div>
+        </div>
+        <br>  
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <?php
+        include("javascript.php");
+    ?>
+  </body>
+</html>
