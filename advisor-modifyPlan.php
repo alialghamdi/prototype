4c155fb7ecdcb36aeec830e09c0feceb1a1c9aab
Omar AlEisa <omar@aleisa.me>
2014-05-15 05:43:10 +0300

    Finished some advisor's screens

--- advisor-modifyPlan.php
+++ advisor-modifyPlan.php
@@ -5,6 +5,24 @@
      <?php
         include("head.php");
       ?>
+      <script>
+        function allowDrop(ev)
+        {
+            ev.preventDefault();
+        }
+
+        function drag(ev)
+        {
+            ev.dataTransfer.setData("Text",ev.target.id);
+        }
+
+        function drop(ev)
+        {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("Text");
+            ev.target.appendChild(document.getElementById(data));
+        }
+      </script>
   </head>
 
   <body>
@@ -15,12 +33,7 @@
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
         <div class="row">
-            <div class="col-md-8">
-                <a href="advisor-viewPlan.php">Back</a>
-                <h3>Plan of student: Ahmad Mohammad<span id="rating-up-not-green" class="glyphicon glyphicon-thumbs-up"></span>  
-                 <span id="rating-down" class="glyphicon glyphicon-thumbs-down"></span></h3>
-                 
-                <br>
+            <div class="col-md-8" id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
                 <table class="table table-striped">
                     <tr>
                         <td>Semster</td>
@@ -34,86 +47,103 @@
                     </tr>
                     <tr>
                         <td>1</td>
-                        <td><button class="btn btn-default">ENGL001</button></td>
                         <td><button class="btn btn-default">MATH001</button></td>
-                        <td><button class="btn btn-default">PYP003</button></td>
-                        <td><button class="btn btn-default">PE001</button></td>
+                        <td><button class="btn btn-default">ENGL001</button></td>
+                        <td><button class="btn btn-default">PHYS001</button></td>
                         <td><button class="btn btn-default">PYP001</button></td>
+                        <td><button class="btn btn-default">PYP003</button></td>
                         
                     </tr>
                     <tr>
                         <td>2</td>
-                        <td><button class="btn btn-default">ENGL001</button></td>
-                        <td><button class="btn btn-default">MATH001</button></td>
-                        <td><button class="btn btn-default">PYP003</button></td>
-                        <td><button class="btn btn-default">PE001</button></td>
-                        <td><button class="btn btn-default">PYP001</button></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td>3</td>
-                        <td><button class="btn btn-default">ENGL001</button></td>
-                        <td><button class="btn btn-default">MATH001</button></td>
-                        <td><button class="btn btn-default">PYP003</button></td>
-                        <td><button class="btn btn-default">PE001</button></td>
-                        <td><button class="btn btn-default">PYP001</button></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td>4</td>
-                        <td><button class="btn btn-default">ENGL001</button></td>
-                        <td><button class="btn btn-default">MATH001</button></td>
-                        <td><button class="btn btn-default">PYP003</button></td>
-                        <td><button class="btn btn-default">PE001</button></td>
-                        <td><button class="btn btn-default">PYP001</button></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td>5</td>
-                        <td><button class="btn btn-default">ENGL001</button></td>
-                        <td><button class="btn btn-default">MATH001</button></td>
-                        <td><button class="btn btn-default">PYP003</button></td>
-                        <td><button class="btn btn-default">PE001</button></td>
-                        <td><button class="btn btn-default">PYP001</button></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td>6</td>
-                        <td><button class="btn btn-default">ENGL001</button></td>
-                        <td><button class="btn btn-default">MATH001</button></td>
-                        <td><button class="btn btn-default">PYP003</button></td>
-                        <td><button class="btn btn-default">PE001</button></td>
-                        <td><button class="btn btn-default">PYP001</button></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                     </tr>
                     <tr>
                         <td>7</td>
-                        <td><button class="btn btn-default">ENGL001</button></td>
-                        <td><button class="btn btn-default">MATH001</button></td>
-                        <td><button class="btn btn-default">PYP003</button></td>
-                        <td><button class="btn btn-default">PE001</button></td>
-                        <td><button class="btn btn-default">PYP001</button></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                     </tr>
                 </table>
-                <div class="row">
-                    <div class="col-md-6">
-                        <button type="modifyPlan.php" class="btn btn-primary btn-lg btn-block">Modify Plan</button>
-                    </div>
-                    <div class="col-md-6">
-                        <button type="editPlan.php" class="btn btn-primary btn-lg btn-block">Approve Plan</button>
-                    </div>
-                </div>
+                
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Changes made</h3>
+                        <h3 class="panel-title">Courses</h3>
                     </div>
                     <div class="panel-body">
-                        <p>
-                            <textarea class="form-control" rows="3">Write a comment...</textarea><br>
-                            <button type="editPlan.php" class="btn btn-primary btn-lg btn-block">Comment</button>
-                        </p>
+                        <button class="btn btn-default" id="drag1" draggable="true" ondragstart="drag(event)">SWE316</button>
+                        <button class="btn btn-default" id="drag2" draggable="true" ondragstart="drag(event)">SWE416</button>
+                        <button class="btn btn-default" id="drag3" draggable="true" ondragstart="drag(event)">SWE205</button>
+                        <button class="btn btn-default" id="drag4" draggable="true" ondragstart="drag(event)">SWE215</button>
+                        <button class="btn btn-default" id="drag5" draggable="true" ondragstart="drag(event)">SWE387</button>
+                        <button class="btn btn-default" id="drag6" draggable="true" ondragstart="drag(event)">SWE363</button>
+                        <button class="btn btn-default" id="drag7" draggable="true" ondragstart="drag(event)">SWE516</button>
+                        <button class="btn btn-default" id="drag8" draggable="true" ondragstart="drag(event)">SWE487</button>
+                        <button class="btn btn-default" id="drag9" draggable="true" ondragstart="drag(event)">ICS102</button>
+                        <button class="btn btn-default" id="drag10" draggable="true" ondragstart="drag(event)">ICS202</button>
+                        <button class="btn btn-default" id="drag11" draggable="true" ondragstart="drag(event)">ICS201</button>
+                        <button class="btn btn-default" id="drag12" draggable="true" ondragstart="drag(event)">ICS410</button>
+                        <button class="btn btn-default" id="drag13" draggable="true" ondragstart="drag(event)">ICS343</button>
+                        <button class="btn btn-default" id="drag14" draggable="true" ondragstart="drag(event)">ICS353</button>
+                        
                     </div>
                 </div>
                 
             </div>
+            
+            
         </div>
+          <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Changes Made</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>
+                            <textarea class="form-control" rows="3">Write a your comments...</textarea><br>
+                        </p>
+                    </div>
+                </div>
+          <a href="advisor-viewPlan.php"  class="btn btn-primary btn-lg btn-block">Commit Edits</a>
         <br>  
       </div>
 
